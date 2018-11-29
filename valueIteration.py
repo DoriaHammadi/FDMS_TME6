40f347bf41f8751515c55db783d97807a6fdecc1
DoriaHammadi <37310697+DoriaHammadi@users.noreply.github.com>
2018-11-29 18:13:34 +0100

    corrigé ..il converge rapidement (5 déplacement en moyenne)

--- valueIteration.py
+++ valueIteration.py
@@ -19,43 +19,42 @@ import copy
 
 class ValueIteration(object):
     """The world's simplest agent!"""
-    def __init__(self, env,gamma,eps = 0.1):
+    def __init__(self, env,gamma,eps = 0.001):
         
         self.env = env 
         self.states, self.p = env.getMDP()
         #sprint(p)
-        value_old = [-10] * len(self.states)
-        self.value_new = [-10] * len(self.states)
-        self.pi = [-10] * len(self.states) 
+        value_old = np.zeros(len(self.states) )
+        self.value_new = np.zeros(len(self.states) ) 
+        self.pi = np.zeros(len(self.states) ) 
         while True :
             value_old = copy.copy(self.value_new)
+            delta = 0
             for s in self.states :
                 if s in self.p.keys(): 
                     self.value_new[self.states[s]] = max ([sum ([dest[0] * (dest[2] + gamma * value_old[self.states[dest[1]]]) 
                                                         for dest in self.p[s][a] ])
                                                   for a in self.p[s] ])
                     
-            delta = np.sum([(n - o)**2 for n, o in zip(self.value_new, value_old)])
+                    delta = max(delta, abs(self.value_new[self.states[s]] - value_old[self.states[s]]))                
             if ( delta < eps):               
                 break 
                 
         #### Meilleur Politique ####
-         ### states qui sont pas dans p.keys() !!!
         for s in self.states :
             if s in self.p.keys():
-                self.pi[self.states[s]] = np.argmax ([sum ([dest[0] * (dest[2] + gamma * self.value_new[self.states[dest[1]]])
-                                                        for dest in self.p[s][a] ])
-                                                      for a in self.p[s]])
-        print(self.pi)
+                act_s = { a : sum ([dest[0] * (dest[2] + gamma * self.value_new[self.states[dest[1]]])
+                            for dest in self.p[s][a] ])
+                        for a in self.p[s]}
+                self.pi[self.states[s]] = max(act_s, key=act_s.get)
                 
                 
     def act(self, observation, reward, done):
         #returner si c une valeur finale
         state = self.states[observation.dumps()]
-        '''print(state)
-        print(self.pi)
-        print(self.value_new)'''
-
+        #print(state)
+        #print(self.pi, self.pi[state])
+        #ssprint(self.value_new)
         return self.pi[state]
         
 
@@ -79,47 +78,42 @@ if __name__ == '__main__':
     env.seed(0)
 
 
-    episode_count = 12
+    episode_count = 10000
     reward = 0
     done = False
     envx.verbose = True
 
-    envx.setPlan("gridworldPlans/plan2.txt",{0:-0.001,3:1,4:1,5:-1,6:-1})
+    envx.setPlan("gridworldPlans/plan0.txt",{0:-0.001,3:1,4:1,5:-1,6:-1})
 
     agent = ValueIteration(envx,0.5)
     #np.random.seed(5)
     rsum=0
-    results = []
+
     for i in range(episode_count):
         ob = env.reset()
 
-        envx.verbose = False
-        '''if i % 100 == 0 and i > 0:
+        if i % 100 == 0 and i > 0:
             envx.verbose = True
         else:
-            envx.verbose = False'''
+            envx.verbose = False
 
         if envx.verbose:
             envx.render(1)
         j = 0
         #print(str(ob))
         while True:
-            #print(agent.states[ob.dumps()])
-            #print(agent.p[ob.dumps()])
-            #print(ob)
             #sprint(ob)
-            #print(len(agent.p[ob.dumps()]))
             action = agent.act(ob, reward, done)
             ob, reward, done, _ = env.step(action)
+            print(done)
             rsum+=reward
             j += 1
             if envx.verbose:
                 envx.render()
             if done:
-                results.append(rsum)
                 print(str(i)+" rsum="+str(rsum)+", "+str(j)+" actions")
                 rsum=0
                 break
-    print(np.mean(results))
+                
     print("done")
     env.close()
